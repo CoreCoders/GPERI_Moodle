d862e190135524b9014d7a8eb4b9acd0f69424ce
rutvik106 <rutvik106@live.com>
2014-03-08 13:13:41 +0530

    Contents class file

---
+++ GPERIMoodle/src/com/example/gperimoodle/Contents.java
@@ -0,0 +1,24 @@
+package com.example.gperimoodle;
+
+public class Contents
+{
+	
+	private static String[] fileName,fileURL;
+
+	public static String[] getFileName() {
+		return fileName;
+	}
+
+	public static void setFileName(String[] fileName) {
+		Contents.fileName = fileName;
+	}
+
+	public static String[] getFileURL() {
+		return fileURL;
+	}
+
+	public static void setFileURL(String[] fileURL) {
+		Contents.fileURL = fileURL;
+	}
+
+}
